1d50a2ffef8a1f50cc91956772220b5fa63dac72
cryptolemming <aliayoub22@gmail.com>
2015-11-08 16:08:15 -0500

    dragsource connected d to move knight to any square using BoardSquare component

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,28 +1,32 @@
 import React, { Component, PropTypes } from 'react';
-import Square from './Square';
+import { DragDropContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+import BoardSquare from './BoardSquare';
 import Knight from './Knight';
 import { canMoveKnight, moveKnight } from './Game';
 
-export default class Board extends Component {
+class Board extends Component {
 	renderSquare(i) {
 		const x = i % 8;
 		const y = Math.floor(i / 8);
-		const black = (x+y) % 2 === 1;
-
-		const [knightX, knightY] = this.props.knightPosition;
-		const piece = (x === knightX && y === knightY) ? 
-			<Knight /> : 
-			null;
 
 		return (
 			<div key = {i}
 				 style = {{ width: '12.5%', height: '12.5%' }}
 				 onClick = {() => this.handleSquareClick(x, y)}>
-				<Square black = {black}>
-					{piece}
-				</Square>
+				<BoardSquare x={x}
+							 y={y}>
+					{this.renderPiece(x, y)}
+				</BoardSquare>
 			</div>
 		);
+	}
+
+	renderPiece(x, y) {
+		const [knightX, knightY] = this.props.knightPosition;
+		if (x === knightX && y === knightY) {
+			return <Knight />;
+		}
 	}
 
 	handleSquareClick(toX, toY) {
@@ -55,3 +59,5 @@ Board.PropTypes = {
 		PropTypes.number.isRequired
 	).isRequired
 };
+
+export default DragDropContext(HTML5Backend)(Board);
--- src/Knight.jsx
+++ src/Knight.jsx
@@ -1,7 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { ItemTypes } from './Constants';
+import { DragSource } from 'react-dnd';
 
-export default class Knight extends Component {
-	render() {
-		return <span>&#9816;</span>;
+const knightSource = {
+	beginDrag(props) {
+		return {};
+	}
+};
+
+function collect(connect, monitor) {
+	return {
+		connectDragSource: connect.dragSource(),
+		isDragging: monitor.isDragging()
 	}
 }
+
+class Knight extends Component {
+	static propTypes = {
+		connectDragSource: PropTypes.func.isRequired,
+		isDragging: PropTypes.bool.isRequired
+	};
+
+	render() {
+		const { connectDragSource, isDragging } = this.props;
+		return connectDragSource(
+			<div style={{
+				opacity: isDragging ? .5 : 1,
+				fontSize: 25,
+				fontWeight: 'bold',
+				cursor: 'move'
+			}}>
+				&#9816;
+			</div>
+		);
+	}
+}
+
+export default DragSource(ItemTypes.KNIGHT, knightSource, collect)(Knight);

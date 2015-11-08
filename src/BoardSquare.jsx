e6f6674f049d12a6da02f67f049505dd315fbfb0
cryptolemming <aliayoub22@gmail.com>
2015-11-08 16:25:10 -0500

    board colors for knight movement

--- src/BoardSquare.jsx
+++ src/BoardSquare.jsx
@@ -5,6 +5,10 @@ import { ItemTypes } from './Constants';
 import { DropTarget } from 'react-dnd';
 
 const squareTarget = {
+	canDrop(props) {
+		return canMoveKnight(props.x, props.y);
+	},
+
 	drop(props) {
 		moveKnight(props.x, props.y);
 	}
@@ -13,35 +17,39 @@ const squareTarget = {
 function collect(connect, monitor) {
 	return {
 		connectDropTarget: connect.dropTarget(),
-		isOver: monitor.isOver()
+		isOver: monitor.isOver(),
+		canDrop: monitor.canDrop()
 	};
 }
 
 class BoardSquare extends Component {
+	renderOverlay(color) {
+		return (
+			<div style={{
+				position: 'absolute',
+				top: 0,
+				left: 0,
+				height: '100%',
+				width: '100%',
+				zIndex: 1,
+				opacity: 0.5,
+				backgroundColor: color,
+			}} />
+		);
+	}
+
 	render() {
-		const { x, y, connectDropTarget, isOver } = this.props;
+		const { x, y, connectDropTarget, isOver, canDrop } = this.props;
 		const black = (x + y) % 2 === 1;
 
 		return connectDropTarget(
-			<div style={{
-				position: 'relative',
-				width: '100%',
-				height: '100%'
-			}}>
+			<div style={{position: 'relative'}}>
 			<Square black={black}>
 				{this.props.children}
 			</Square>
-			{isOver &&
-				<div style={{
-					position: 'absolute',
-					top: 0,
-					left: 0,
-					height: '100%',
-					zIndex: 1,
-					opacity: .5,
-					backgroundColor: 'blue'
-				}} />
-			}
+				{isOver && !canDrop && this.renderOverlay('red')}
+				{!isOver && canDrop && this.renderOverlay('yellow')}
+				{isOver && canDrop && this.renderOverlay('green')}
 			</div>
 		);
 	}
@@ -50,7 +58,8 @@ class BoardSquare extends Component {
 BoardSquare.PropTypes = {
 	x: PropTypes.number.isRequired,
 	y: PropTypes.number.isRequired,
-	isOver: PropTypes.bool.isRequired
+	isOver: PropTypes.bool.isRequired,
+	canDrop: PropTypes.bool.isRequired
 };
 
 export default DropTarget(ItemTypes.KNIGHT, squareTarget, collect)(BoardSquare);
